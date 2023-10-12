b7cfc75f2e3106ebfe8ea9de4456b230ef3104a4
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-10-12 21:31:54 +0200

    add plots for step 1

--- src/main.py
+++ src/main.py
@@ -6,22 +6,26 @@ R R [possiblement, probablement, éventuellement, il se pourrait, il y a une pos
 """
 
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 #import math
 #import pandas as pd
 import numpy as np
 import scipy as sp
 
 #t = np.arange(0, 5, 0.01)
-T = 5
-t = np.linspace(0, 5, T*1000, endpoint=True)
+# Chirp's end time
+T_chirp_duration = 1e-1  # 0.1 ms - 0.4 ms
+T = 100 # 100ms
+Number_of_samples = 2**18 # 2**18 # 262144 samples
+#t = np.linspace(0, T_chirp_duration, Number_of_samples, endpoint=True)
+t = np.linspace(0, T, Number_of_samples, endpoint=True)
 
 B_freq_range = 200e6 # 200 MHz
 f_c = 24*10e9 # Carrier frequency 24 GHz
 F_sampling_freq = 512e6  # 512 MHz
-Num_samples = 2**18
 # chirp duration T = 0.1 - 0.4 ms
-T_chirp_duration = 2e-4 # 0.2 ms
-Beta_slope = B_freq_range/T # B = Beta*T
+
+Beta_slope = B_freq_range/T_chirp_duration # B = Beta*T
 
 def f_i(t):
     return Beta_slope*t
@@ -29,12 +33,35 @@ def f_i(t):
 def phi_i(t):
     return np.pi*Beta_slope*t**2
 
-def s(t):
+def s_with_carrier(t):
     return np.cos(2*np.pi*f_c*t + phi_i(t))
 
+def s(t):
+    return np.cos(phi_i(t))
+
 #f_i = [sp.signal.sawtooth(2 * np.pi * 5 * t) for t in t]
-#plt.plot(t, f_i)
+plt.plot(t, f_i(t))
+plt.grid()
+plt.legend(['$f_i$'])
+plt.xlabel('Time (ms)')
+plt.ylabel('Frequency (Hz)')
+plt.title('Instantaneous frequency')
+#go.Figure(data=go.Scatter(x=t, y=f_i(t))).show()
+
 #plt.plot(t, B*sp.signal.sawtooth(2 * np.pi * t))
 
 # math.cos(2*math.pi*t)
 #plt.plot(t, np.cos(2 * np.pi * 5 * t))
+
+plt.show()
+
+plt.plot(t, s(t))
+plt.grid()
+plt.legend(['$s(t)$'])
+plt.xlabel('Time (ms)')
+plt.ylabel('Amplitude')
+plt.title('FM transmitted signal')
+#go.Figure(data=go.Scatter(x=t, y=s(t))).show()
+
+plt.show()
+
