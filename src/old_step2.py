65903db3e544c32eed8954c51712b82128e68efe
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-11-27 13:00:56 +0100

    rename step2

---
+++ src/old_step2.py
@@ -0,0 +1,79 @@
+"""
+ELEC-H311 - Joint Communications and Radar simulation project
+Lucas Placentino
+Salman Houdaibi
+"""
+
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# import math
+# import pandas as pd
+import numpy as np
+import scipy as sp
+import step1
+
+"""
+    Step 2: Radar processing
+
+    Generate a Range Doppler Map (RDM) of target(s)
+    Target range is max 20m, and speed is max 2m/s with respect to the radar
+"""
+
+T_chirp_duration = 2e-4  # chirp duration: 0.1 ms to 0.4 ms
+
+fc_carrier_freq = 24e9  # f_c
+
+B_freq_range = 200e6  # 200 MHz
+
+Fs_radar_sampling_freq = 2e6  # 2 MHz
+
+F_sim_sampling_freq = 512e6  # 512 MHz
+
+N_fast_time_fft_size = 512
+
+Number_guard_samples = 5
+
+K_slow_time_fft_size = 256
+
+def main():
+
+    # --- 1. --- Generate the FMCW signal composed of K chirps
+
+    single_chirp = step1.signal_baseband  # used with step1.t
+
+    FMCW_over_K_chirps = K_slow_time_fft_size * single_chirp
+
+    """
+    plt.plot(step1.t*K_slow_time_fft_size, FMCW_over_K_chirps.real, label="Real")
+    plt.plot(step1.t*K_slow_time_fft_size, FMCW_over_K_chirps.imag, label="Imaginary", linestyle="--")
+    plt.grid()
+    plt.title("FMCW signal over K chirps")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.legend()
+
+    plt.show()
+    """
+
+
+    # --- 2. --- Simulate the impact of the single-target channel on the FMCW signal (the extension to a multi-target channel is obviously the sum of the target contributions)
+
+    # single target: shifted signal (shifted in time by a constant delay in freq by a constant Doppler). Neglect amplitude changes due to losses over the distance.
+
+    # multiple targets: multiple persons moving at different speeds and distances from the radar. The signal is the sum of the shifted signals of each target.
+
+
+    # --- 3. --- Implement the radar processing: mixing with the transmitted signal, sampling at F_s, S/P conversion,FFT over the fast and slow time dimensions
+
+    # mixing signals ? mixed signal sampled at F_s. S/P conversion: sampled signal from serial format to parallel format forming blocks of samples. Then 2D FFT (over the fast and slow time dimensions) of the parallel converted signal => generates the RDM.
+    # amplitude peaks in the RDM are the targets.
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- src/step2.py
+++ src/step2.py
@@ -1,79 +1,68 @@
-"""
-ELEC-H311 - Joint Communications and Radar simulation project
-Lucas Placentino
-Salman Houdaibi
-"""
-
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-
-# import math
-# import pandas as pd
 import numpy as np
-import scipy as sp
-import step1
+import matplotlib.pyplot as plt
+import scipy.fft as sft
 
 """
-    Step 2: Radar processing
-
-    Generate a Range Doppler Map (RDM)
-    Target range is max 20m, and speed is max 2m/s with respect to the radar
+    Step 2: PSK Modulation of FMCW Signal
 """
 
-T_chirp_duration = 2e-4  # chirp duration: 0.1 ms to 0.4 ms
+# Paramètres existants
+T_chirp_duration = 2e-4  # Durée du chirp
+Number_of_samples = 2**18
+B_freq_range = 200e6
+f_c = 24e9
+F_sampling_freq = 512e6
+Beta_slope = B_freq_range / T_chirp_duration
+t = np.linspace(0, T_chirp_duration, Number_of_samples, endpoint=True)
 
-fc_carrier_freq = 24e9  # f_c
+# Fonction du signal de baseband
+def s_baseband(t):
+    return np.exp(1j * np.pi * Beta_slope * (t**2))
 
-B_freq_range = 200e6  # 200 MHz
+# Génération de la séquence PSK (exemple avec BPSK)
+N_symbols = 100  # Nombre de symboles PSK
+symbols = np.random.choice([-1, 1], N_symbols)  # Séquence BPSK
+symbol_rate = 1 / (T_chirp_duration / N_symbols)  # Taux de symboles
+samples_per_symbol = int(F_sampling_freq / symbol_rate)  # Échantillons par symbole
+psk_sequence = np.repeat(symbols, samples_per_symbol)
 
-Fs_radar_sampling_freq = 2e6  # 2 MHz
+# Redimensionner la séquence PSK pour qu'elle corresponde à la durée du chirp
+psk_sequence = np.pad(psk_sequence, (0, Number_of_samples - len(psk_sequence)), 'constant')
 
-F_sim_sampling_freq = 512e6  # 512 MHz
+# Modulation PSK du signal FMCW
+signal_baseband = s_baseband(t)
+signal_modulated = signal_baseband * psk_sequence
 
-N_fast_time_fft_size = 512
-
-Number_guard_samples = 5
-
-K_slow_time_fft_size = 256
+# Transformation de Fourier
+fft_signal_modulated = sft.fft(signal_modulated)
+fft_freq = sft.fftfreq(Number_of_samples, d=1 / F_sampling_freq)
+fft_shifted_modulated = sft.fftshift(fft_signal_modulated)
+fft_freq_shifted = sft.fftshift(fft_freq)
 
 def main():
+    # Affichage
+    plt.figure(figsize=(12, 10))
 
-    # --- 1. --- Generate the FMCW signal composed of K chirps
-
-    single_chirp = step1.signal_baseband  # used with step1.t
-
-    FMCW_over_K_chirps = K_slow_time_fft_size * single_chirp
-
-    """
-    plt.plot(step1.t*K_slow_time_fft_size, FMCW_over_K_chirps.real, label="Real")
-    plt.plot(step1.t*K_slow_time_fft_size, FMCW_over_K_chirps.imag, label="Imaginary", linestyle="--")
+    # Signal FMCW modulé en PSK
+    plt.subplot(3, 1, 1)
+    plt.plot(t, signal_modulated.real, label="Real")
+    plt.plot(t, signal_modulated.imag, label="Imaginary", linestyle="--")
     plt.grid()
-    plt.title("FMCW signal over K chirps")
+    plt.title("PSK Modulated FMCW Signal")
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
     plt.legend()
 
+    # Spectre du signal FMCW modulé
+    plt.subplot(3, 1, 2)
+    plt.plot(fft_freq_shifted, np.abs(fft_shifted_modulated))
+    plt.grid()
+    plt.title("Spectrum of PSK Modulated FMCW Signal")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
+
+    plt.tight_layout()
     plt.show()
-    """
-
-
-    # --- 2. --- Simulate the impact of the single-target channel on the FMCW signal (the extension to a multi-target channel is obviously the sum of the target contributions)
-
-    # single target: shifted signal (shifted in time by a constant delay in freq by a constant Doppler). Neglect amplitude changes due to losses over the distance.
-
-    # multiple targets: multiple persons moving at different speeds and distances from the radar. The signal is the sum of the shifted signals of each target.
-
-
-    # --- 3. --- Implement the radar processing: mixing with the transmitted signal, sampling at F_s, S/P conversion,FFT over the fast and slow time dimensions
-
-    # mixing signals ? mixed signal sampled at F_s. S/P conversion: sampled signal from serial format to parallel format forming blocks of samples. Then 2D FFT (over the fast and slow time dimensions) of the parallel converted signal => generates the RDM.
-    # amplitude peaks in the RDM are the targets.
-
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
