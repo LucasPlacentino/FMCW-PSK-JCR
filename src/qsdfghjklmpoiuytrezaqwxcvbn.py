6e2e2c1a9b8011ef1529439bc4e79e7cf7ce7489
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-12-20 16:00:56 +0100

    Update qsdfghjklmpoiuytrezaqwxcvbn.py

--- src/qsdfghjklmpoiuytrezaqwxcvbn.py
+++ src/qsdfghjklmpoiuytrezaqwxcvbn.py
@@ -1,16 +1,22 @@
+# Importing necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fft as sft
 
+# Importing a module named "step1" from the same directory
 import step1
 
+# Creating and manipulating numpy arrays
 a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 b = np.reshape(a, (2, 6))
 c = np.transpose(b)
+
+# Printing the arrays
 print(a)
 print(b)
 print(c)
 
+# Defining various parameters for the radar system
 T_chirp_duration = 2e-4
 Number_of_samples = 2**18
 B_freq_range = 200e6
@@ -24,14 +30,22 @@ c = 3e8
 max_range = 1000
 max_speed = 100
 tau_max = (2 * max_range) / c
+
+# Taking user input for the number of targets
 number_of_targets = int(input("Enter number of targets (int), e.i 5 : "))
+
+# Defining the guard samples
 guard_samples = 5
 
+# Calculating the radar theoretical maximum range
 radar_max_range = (c * F_radar_sampling_freq) / (2 * Beta_slope)
 print("radar theoretical maximum range ? : ", radar_max_range, "m")
 
+# Generating the FMCW signal for each chirp
 single_chirp_signal = step1.signal_baseband
 FMCW_over_K_chirps = np.tile(single_chirp_signal, K_slow_time_fft_size)
+
+# Generating the time vector for each chirp
 t_over_k_chirps = np.linspace(
     0,
     T_chirp_duration * K_slow_time_fft_size,
@@ -39,16 +53,21 @@ t_over_k_chirps = np.linspace(
     endpoint=True,
 )
 
+# Calculating the number of samples per chirp
 T_r = T_chirp_duration - tau_max
 N_samples_per_chirp = F_radar_sampling_freq * T_r
 print("N_samples_per_chirp: ", N_samples_per_chirp, "sould be 512?")
 
+# Defining the target delays and velocities
 target_delays = [tau_max*0.2, tau_max]
 target_velocities = [-max_speed*0.1, max_speed*0.1]
+
+# Printing the target delays and velocities
 for i in range(number_of_targets):
     print("Target", i + 1, "- delay:", "{:.2f}".format(target_delays[i] * 1e9), "ns", end=", ")
     print("velocity:", "{:.2f}".format(target_velocities[i]), "m/s")
 
+# Function to calculate the target contribution to the received signal
 def target_contribution(target_delay, target_velocity):
     R_0_initial_range = c * target_delay / 2
     doppler_freq = 2 * target_velocity * f_c / c
@@ -70,22 +89,27 @@ def target_contribution(target_delay, target_velocity):
 
     return target_signal
 
+# Calculating the total target contribution to the received signal
 signal_target = sum(target_contribution(target_delays[i], target_velocities[i]) for i in range(number_of_targets))
 print("signal_target shape",signal_target.shape)
 
+# Calculating the total number of samples and the sampling interval
 total_number_of_samples = (T_chirp_duration*K_slow_time_fft_size) * 1/F_radar_sampling_freq
 sampling_interval = int(len(signal_target) / total_number_of_samples)
 sampled_signal = signal_target
 print("sampled_signal: ", sampled_signal, "length: ", len(sampled_signal))
 
+# Converting the sampled signal into a 2D array
 sp_converted_signal = np.reshape(sampled_signal, (K_slow_time_fft_size, N_fast_time_fft_size))
 sp_converted_signal = np.transpose(sp_converted_signal)
 print("sp_converted_signal shape:",sp_converted_signal.shape,"(height, width), or (rows, columns)")
 
+# Performing fast time and slow time FFT
 fast_time_fft = np.fft.fft(sp_converted_signal, axis=0)
 slow_time_fft = np.fft.fft(fast_time_fft, axis=1)
 print(slow_time_fft.shape)
 
+# Plotting the Range-Doppler Map (RDM)
 plt.figure(figsize=(10, 6))
 plt.imshow(
     np.abs(slow_time_fft),
@@ -99,15 +123,20 @@ plt.ylabel("Range Bins")
 plt.colorbar(label="Amplitude")
 plt.show()
 
+# Printing the range and Doppler resolutions
 print("Range resolution: ", c / (2 * B_freq_range), " m?")
 print("Doppler resolution: ", 1 / (K_slow_time_fft_size * T_chirp_duration), " Hz?")
+
+# Printing the relevance of radar parameters for the considered scenario
 print("--- Relevance of radar parameters for the considered scenario: ---")
 print("The frequency range B is the bandwidth of the transmitted signal. It directly determines the range resolution, inversely-proportional. For our case, it is set to 200 MHz.")
 print("The chirp duration T_chirp_duration is the time duration of a chirp. It directly determines the Doppler frequency resolution, inversely-proportional. For our case, it is set between 0.1 and 0.4 ms.")
 print("The number of chirps K_slow_time_fft_size is the number of chirps in a sequence. It directly determines the Doppler frequency resolution, inversely-proportional. For our case, it is set to 256.")
 
+# Function to plot the Range-Doppler Map (RDM)
 def plot():
     plt.show()
 
+# Calling the plot function if the script is run directly
 if __name__ == "__main__":
     plot()
